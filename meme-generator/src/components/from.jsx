4d97fcb83331aea7967437c9395c205403d8b0f1
yudiz_jayk <jay.karavadra@yudiz.com>
2024-01-20 17:19:28 +0530

    Complete meme generator project

--- meme-generator/src/components/from.jsx
+++ meme-generator/src/components/from.jsx
@@ -1,24 +1,35 @@
-import { useState } from 'react'
-
-const {data} =require('../memesData')
+import { useEffect, useState } from 'react'
 
 
 function Form() {
+    const [meme, setMeme] = useState({ firstName: "", lastName: "", imageUrl: "https://i.imgflip.com/30b1gx.jpg" })
 
-    const [meme,setMeme] = useState({firstName:"", lastName:"",imageUrl:"https://i.imgflip.com/30b1gx.jpg"})
-    
+    const [memeData, setMemeData] = useState([])
+
+
+    useEffect(() => {
+        async function fetchData() {
+            let data = await fetch('https://api.imgflip.com/get_memes')
+            data = await data.json()
+            console.log(data);
+            
+            setMemeData(data.data.memes)
+        }
+
+        fetchData()
+    },[])
+
     function changeStatus(e) {
-        const {name,value} =e.target
-        console.log({name,value});
-       setMeme((prevState)=>{
-          return {...prevState,[name]:value}
-       })   
+        const { name, value } = e.target
+        setMeme((prevState) => {
+            return { ...prevState, [name]: value }
+        })
     }
 
     function generateMemeImage() {
-        setMeme((prevState)=>{
-            return {...prevState,imageUrl:data[Math.floor(Math.random() * 100) ].url}
-         })   
+        setMeme((prevState) => {
+            return { ...prevState, imageUrl: memeData[Math.floor(Math.random() * 100)].url }
+        })
     }
 
 
@@ -47,7 +58,7 @@ function Form() {
             </div>
 
             <div className="meme">
-                <img src={meme.imageUrl} alt="meme img`"  />
+                <img src={meme.imageUrl} alt="meme img`" />
                 <h1 className="meme--text top">{meme.firstName}</h1>
                 <h1 className="meme--text bottom">{meme.lastName}</h1>
             </div>
